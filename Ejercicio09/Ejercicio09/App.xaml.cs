6bf50e886166371d05941e33aef6059cd68ea40f
VlexvnderF <alexander.flores.r@tecsup.edu.pe>
2022-10-19 23:54:10 -0500

    Agregar archivos de proyecto.

---
+++ Ejercicio09/Ejercicio09/App.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Ejercicio09
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            //MainPage = new MainPage();
+            //MainPage = new InheritanceDemo();
+            //MainPage = new DynamicDemo();
+            //MainPage = new ExplicitControlDemo();
+            //MainPage = new ExplicitDemo();
+            //MainPage = new GlobalDemo();
+            //MainPage = new ImplicitControlDemo();
+            //MainPage = new ImplicitDemo();
+            MainPage = new CssXama();
+
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ Ejercicio09/Ejercicio09/CustomEntry.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace Ejercicio09
+{
+    internal class CustomEntry: Entry
+    {
+
+    }
+}
